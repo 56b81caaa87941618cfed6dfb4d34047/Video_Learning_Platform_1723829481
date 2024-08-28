c62905ee4ea64098af7b9c4ba3faa964adf8909e
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-28 04:40:18 +0000

    Code Generation

--- components/simple_header_component_1723829483.js
+++ components/simple_header_component_1723829483.js
@@ -30,6 +30,11 @@ Vue.component("simple_header_component_1723829483", {
                         <li id="nav-item-4"><a href="#" class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">About Us</a></li>
                     </ul>
                 </div>
+                <div class="flex-1 flex justify-end items-center ml-4">
+                    <button id="change-text-button" @click="changeText" class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded shadow-md transition duration-300 ease-in-out">
+                        Change Text
+                    </button>
+                </div>
             </div>
         </nav>
     </header>`,
@@ -37,6 +42,14 @@ Vue.component("simple_header_component_1723829483", {
         return {
             expanded: false,
             tab: null,
+            buttonText: 'Change Text'
         };
     },
+    methods: {
+        // Change Text method
+        changeText() {
+            this.buttonText = this.buttonText === 'Change Text' ? 'Text Changed' : 'Change Text';
+        }
+        // End of Change Text method
+    }
 });
